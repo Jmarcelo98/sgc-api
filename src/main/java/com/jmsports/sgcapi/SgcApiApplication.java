3c6908051c09e726b168be6dbfdfa878b410df3a
João Marcelo <joaomarcelo588@gmail.com>
2023-11-14 17:33:51 -0300

    Projeto criado

---
+++ src/main/java/com/jmsports/sgcapi/SgcApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmsports.sgcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmsports/sgcapi/SgcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmsports.sgcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
